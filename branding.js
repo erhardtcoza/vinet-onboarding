c1b1054474aaa15fba76817dd6df368a3805d36a
Erhardt <erhardt@erhardt.co.za>
2025-10-29 18:35:10 +0200

    remove

--- branding.js
+++
@@ -1,6 +0,0 @@
-export function ipAllowed(request) {
-  // VNET 160.226.128.0/20 (128..143)
-  const ip = request.headers.get("CF-Connecting-IP") || "";
-  const [a,b,c] = ip.split(".").map(Number);
-  return a===160 && b===226 && c>=128 && c<=143;
-}
